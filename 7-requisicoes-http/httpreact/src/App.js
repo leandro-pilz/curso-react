2e357148af02b7f49afb44e01fde4414a7a39eb7
Leandro Pilz <leandro.pilz@bem.inf.br>
2023-10-26 21:34:04 -0300

    Capítulo 7 HTTP com React

---
+++ 7-requisicoes-http/httpreact/src/App.js
@@ -0,0 +1,99 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
+
+const url = "http://localhost:3000/products";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  // 4 - custom
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // 1- Resgatando dados
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+
+  //   fetchData();
+  // }, []);
+
+  //Adição de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const product = {
+      name,
+      price,
+    };
+
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+
+    // // 3 - Carregamento dinâmico
+    // const addedProduct = await res.json();
+
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
+  };
+
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!error && (
+        <ul>
+          {items &&
+            items.map((product) => (
+              <li key={product.id}>
+                {product.name} - R$ {product.price}
+              </li>
+            ))}
+        </ul>
+      )}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {/* 7 - State de loading no post */}
+          {loading && <input type="submit" value="Aguarde" disabled />}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 7-requisicoes-http/httpreact/src/hooks/useFetch.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+// 4 - Custom hook
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  //5 - Refatorando post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  // 6 - loading
+  const [loading, setLoading] = useState(false);
+
+  // 7 - Tratando erros
+  const [error, setError] = useState(null);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod(method);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // 6 - Loading
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setError("Houve algum erro ao carregar os dados!");
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [url, callFetch]);
+
+  // 5 - Refatorando post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+        const res = await fetch(...fetchOptions);
+        const json = await res.json();
+        setCallFetch(json);
+      }
+    };
+
+    httpRequest();
+  }, [config, method, url]);
+
+  return { data, httpConfig, loading, error };
+};
