d794d8f8fbdb4734b7bf876a3936f0026e10a995
Leandro Pilz <leandro.pilz@bem.inf.br>
2023-10-30 07:52:38 -0300

    Finalizada a seção 10 - Projeto com firebase - Miniblog

--- 10-projeoto-miniblog/miniblog/src/App.js
+++ 10-projeoto-miniblog/miniblog/src/App.js
@@ -20,6 +20,7 @@ import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import CreatePost from "./pages/CreatePost/CreatePost";
+import EditPost from "./pages/EditPost/EditPost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
 import Post from "./pages/Post/Post";
@@ -58,6 +59,10 @@ function App() {
               <Route
                 path="/register"
                 element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
               />
               <Route
                 path="/posts/create"
