be992b7d776889ec75bb8b4238dcf54ab5b78ba1
Leandro Pilz <leandro.pilz@bem.inf.br>
2023-11-01 22:42:07 -0300

    Seção 18 - Apêndice projeto react com typescript

---
+++ 18-projeto-react-typescript/todo_react/src/components/Fotter.tsx
@@ -0,0 +1,13 @@
+import styles from "./Fotter.module.css";
+
+const Fotter = () => {
+  return (
+    <footer className={styles.fotter}>
+      <p>
+        <span>React + TS Todo</span> @2023
+      </p>
+    </footer>
+  );
+};
+
+export default Fotter;
